6f1be581acade27d8834cebb79938f6d5963f708
choushen <jacob.mckenzie@icloud.com>
2024-08-06 22:54:32 +0100

    Added case for . any character, the end of a line, empty pattern and default  case for  if there is no match

--- main.py
+++ main.py
@@ -7,20 +7,36 @@ def match(text, pattern):
         return False
     
     if len(pattern) == 1 and pattern in text:
-        print(f"Matched single letter{text} with pattern: {pattern}")
+        print(f"Matched single letter '{text}' with pattern: '{pattern}'")
         return True
     
     if pattern[0] == '^':
-        print(f"Matched start of line {pattern[0]} - moving to next character")
+        print(f"Matched start of line character '{pattern[0]}' - moving to next character")
         return match(pattern[1:], text)
     
-    # for i in range(len(text) + 1):
-    #     print(f"Trying to match from text position {i}: '{text[i:]}'")
-    #     if match_here(pattern, text[i:]):
-    #         return True
-    # return False
+    for i in range(len(text) + 1):
+        print(f"Trying to match from text position {i}: '{text[i:]}'")
+        if match_here(pattern, text[i:]):
+            return True
+    return False
+# match() end
 
+def match_here(text, pattern):
+    if not pattern:
+        print("Pattern is empty")
+        return True
     
+    if pattern[0] == '$' and not pattern[1:]:
+        print("Matched end of line character")
+        return not text
+    
+    if (text and pattern[0] == '.') or (pattern[0] == text[0]):
+        print(f"Matched character '{pattern[0]}' with text '{text[0]}'")
+        return match_here(text[1:], pattern[1:])
+    
+    print(f"Failed to match character '{pattern[0]}' with text '{text[0]}'")
+    return False
+# match_here() end
 
 def main():
     if len(sys.argv) < 3:
@@ -29,6 +45,7 @@ def main():
     
     print(f"Matching text: {sys.argv[1]} with pattern: {sys.argv[2]}")
     match(sys.argv[1], sys.argv[2])
+# main() end
 
 if __name__ == "__main__":
     main()
